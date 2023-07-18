f2951c7c723fba3bc36f123dd07b10738d067fdd
filipe-silva-27 <filipesprogramming@gmail.com>
2023-07-18 08:46:28 +0100

    feat: IntStreams completed

--- src/test/java/com/amigoscode/examples/IntStreams.java
+++ src/test/java/com/amigoscode/examples/IntStreams.java
@@ -19,9 +19,23 @@ public class IntStreams {
     @Test
     public void rangeIteratingLists() throws Exception {
         List<Person> people = MockData.getPeople();
+
+        IntStream.range(0, people.size()).forEach(personId -> System.out.println(people.get(personId)));
+
+        //ainda não percebi a aplicabilidade disto, uma vez que podemos fazer
+        //que como é people.size() até nem precisamos do limit
+
+        //pelo que percebi a aplicabilidade é a mesma quando discutimos
+        //for vs foreach aka for ( ; ; ) ou for ( : )
+
+        people.stream().limit(people.size()).forEach(System.out::println);
     }
 
     @Test
     public void intStreamIterate()  {
+        //bem, o iterate parece fazer o mesmo que range
+        IntStream.iterate(0, value -> ++value)
+                .limit(11)  // Exclusive, so this loop will print out 10 but not 11!
+                .forEach(System.out::println);
     }
 }
