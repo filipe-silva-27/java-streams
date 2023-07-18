21f42265cad5c17377aa6733f014bf9947db118e
filipe-silva-27 <filipesprogramming@gmail.com>
2023-07-18 13:10:36 +0100

    feat: JoiningStrings completed

--- src/test/java/com/amigoscode/examples/JoiningStrings.java
+++ src/test/java/com/amigoscode/examples/JoiningStrings.java
@@ -11,12 +11,21 @@ public class JoiningStrings {
     public void joiningStrings() throws Exception {
         List<String> names = List.of("anna", "john", "marcos", "helena", "yasmin");
         // "Anna, John, Marcos, Helena, Yasmin"
+
     }
 
     @Test
     public void joiningStringsWithStream() throws Exception {
         List<String> names = List.of("anna", "john", "marcos", "helena", "yasmin");
         // "Anna, John, Marcos, Helena, Yasmin"
+
+        final String joinedStrings = names.stream().reduce("", (acc, string) -> acc.concat(string));
+        System.out.println(joinedStrings);
+
+        final String joinedStrings2 = names.stream().reduce("", String::concat);
+        System.out.println(joinedStrings2);
+
+        assert joinedStrings2.equals(joinedStrings);
 
     }
 
